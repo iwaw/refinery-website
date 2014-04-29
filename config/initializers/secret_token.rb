79a9b9201c0a271e9f5216c90f4716c5e97d1f85
Sarah Lensing <sarahlensing@gmail.com>
2014-04-29 11:00:55 +0700

    Basic refinery project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IwawWebsite::Application.config.secret_token = '11f8f554428a2da8212653cb2f2289b81a2e43fc7ff1cc841f621420a5914885efa079183eee2d5fba1fde17fd8c6e817d1167820b2aecd312123d7adc77f756'
