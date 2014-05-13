4d7a0f1fd3eafc54c996100bc8ebe19a51de3647
Sarah Lensing <sarahlensing@gmail.com>
2014-05-13 15:29:19 +0700

    Update css

--- config/initializers/refinery/core.rb
+++ config/initializers/refinery/core.rb
@@ -20,7 +20,7 @@ Refinery::Core.configure do |config|
   # config.base_cache_key = :refinery
 
   # Site name
-  config.site_name = "iW@W"
+  config.site_name = "IW@W - International Women @ Work"
 
   # This activates Google Analytics tracking within your website. If this
   # config is left blank or set to UA-xxxxxx-x then no remote calls to
